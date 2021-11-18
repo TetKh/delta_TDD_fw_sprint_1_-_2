2551f186cafb9291416fb2889f31a113b2a45ac6
Olga T <vlada.88@inbox.ru>
2021-11-18 18:48:42 -0500

    Added tests for contact info section

---
+++ src/test/java/pages/ContactInfoPage.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class ContactInfoPage {
+
+    public ContactInfoPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "countryCode-1-button")
+    public WebElement countryRegion;
+
+    @FindBy(id = "aType-1-button")
+    public WebElement addressType;
+
+    @FindBy(id = "addr1-1")
+    public WebElement addressLine1;
+
+    @FindBy (id = "addr2-1")
+    public WebElement addressLine2;
+
+    @FindBy (name= "customerDo.addresses[0].addressLine4")
+    public WebElement city;
+
+    @FindBy(id = "stateProv-1-button")
+    public WebElement stateProvince;
+
+    @FindBy(id = "postal-1")
+    public WebElement postalCode;
+
+    @FindBy(xpath = "phoneCountryCode-button")
+    public WebElement countryRegionCode;
+
+    @FindBy(id = "phoneAreaCode")
+    public WebElement areaCode;
+
+    @FindBy(id = "requiredPhoneNumber")
+    public WebElement phoneNo;
+
+    @FindBy(id = "basicInfoEmailAddress")
+    public WebElement email;
+
+    @FindBy(id = "requiredEmail2")
+    public WebElement confirmEmail;
+
+    @FindBy(id = "basicInfoTravelerNo")
+    public WebElement travelNo;
+
+
+}
---
+++ src/test/java/tests/ContactInfoTest.java
@@ -0,0 +1,80 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.BasicInfoPage;
+import pages.ContactInfoPage;
+import utilities.SeleniumUtils;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class ContactInfoTest {
+
+    ContactInfoPage contactInfoPage;
+    Faker faker;
+
+    @Test
+    public void verifyCountryRegionDropDown() {
+        new SignUpButtonTest().SingUpButtonTest();
+        new ContactInfoPage().countryRegion.click();
+
+    }
+
+        @Test
+        public void verifyAddressTypeDropDown() {
+            new SignUpButtonTest().SingUpButtonTest();
+            new ContactInfoPage().addressType.click();
+
+        }
+
+    @Test
+    public void verifyAddressCredentials(){
+
+        String email = faker.funnyName().name();
+        new SignUpButtonTest().SingUpButtonTest();
+        Faker faker = new Faker();
+        new ContactInfoPage().addressLine1.sendKeys(faker.address().streetAddress());
+        new ContactInfoPage().addressLine2.sendKeys(faker.address().streetAddressNumber());
+        new ContactInfoPage().city.sendKeys(faker.address().city());
+        new ContactInfoPage().postalCode.sendKeys(faker.address().zipCode());
+        new ContactInfoPage().areaCode.sendKeys(faker.phoneNumber().phoneNumber());
+        new ContactInfoPage().phoneNo.sendKeys(faker.phoneNumber().phoneNumber());
+        new ContactInfoPage().email.sendKeys(email);
+        new ContactInfoPage().confirmEmail.sendKeys(email);
+
+
+    }
+
+            @Test
+            public void verifyStateProvinceDropDown() {
+                new SignUpButtonTest().SingUpButtonTest();
+                new ContactInfoPage().stateProvince.click();
+            }
+
+//    @Test
+//    public void verifyFullNameInValidCredentials(){
+//        new SignUpButtonTest().SingUpButtonTest();
+//        Faker faker = new Faker();
+//        new ContactInfoPage().addressLine1.sendKeys(faker.chuckNorris().fact());
+//        new ContactInfoPage().addressLine2.sendKeys(faker.phoneNumber().extension());
+//        new ContactInfoPage().city.sendKeys(faker.slackEmoji().celebration());
+//        //assert
+//    }
+
+    @Test
+    public void verifyCountryRegionCodeDropDown(){
+        new SignUpButtonTest().SingUpButtonTest();
+        new ContactInfoPage().countryRegionCode.click();
+
+        // assert?
+
+    }
+
+
+}
