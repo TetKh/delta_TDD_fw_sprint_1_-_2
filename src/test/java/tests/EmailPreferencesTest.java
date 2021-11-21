498df5af46a58f6f8d55e240ba92d213f5bc20b4
Magda <magdarojs@gmail.com>
2021-11-20 20:58:01 -0500

    3rd commit - include assert method for EmailPreferencesTest

--- src/test/java/tests/EmailPreferencesTest.java
+++ src/test/java/tests/EmailPreferencesTest.java
@@ -3,6 +3,7 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.EmailPreferencesPage;
+import utilities.Driver;
 import utilities.SeleniumUtils;
 
 public class EmailPreferencesTest extends TestBase{
@@ -15,7 +16,21 @@ public class EmailPreferencesTest extends TestBase{
 
       new SignUpButtonTest().SingUpButtonTest();
       new EmailPreferencesPage().emailPreferenceSelectButton.click();
-        SeleniumUtils.waitFor(10);
-        Assert.assertTrue(  new EmailPreferencesPage().emailPreferenceSelectButton.isSelected());
+      SeleniumUtils.waitFor(10);
+
+      // Option 1
+//      String expected = "✓ SELECTED";
+//      String actual = new EmailPreferencesPage().emailPreferenceSelectButton.getText();
+//      Assert.assertEquals(ac,expected);
+
+      // Option 2
+
+//        String expected = "SELECTED";
+//        String pageSource = Driver.getDriver().getPageSource();
+//        Assert.assertTrue(pageSource.contains(expected));
+
+
+// Your method
+ //Assert.assertTrue(  new EmailPreferencesPage().emailPreferenceSelectButton.isSelected());
     }
 }
