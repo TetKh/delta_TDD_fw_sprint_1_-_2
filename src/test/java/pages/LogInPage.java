c088317c7e86851403be2eb2be4e89d7512fbe86
Magda <magdarojs@gmail.com>
2021-11-27 11:11:17 -0500

    1st commit

---
+++ src/test/java/pages/LogInPage.java
@@ -0,0 +1,75 @@
+package pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class LogInPage {
+    public LogInPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//button[@class='login-btn btn btn-danger']")
+    public WebElement logInButton;
+
+    @FindBy(id = "userId")
+    public WebElement userRow;
+
+    @FindBy(id = "password")
+    public WebElement passwordRow;
+
+    @FindBy(xpath = "//button[@type=\"submit\"]")
+    public WebElement redLogInButton;
+
+    @FindBy(xpath = "//a [@class=\"continue-link\"]")
+    public WebElement errorMessage;
+
+
+    @FindBy(xpath = "//label[@for='persistentLogin_CheckBox']")
+    public WebElement checkbox;
+
+
+    @FindBy(xpath = "//a[@class='persistent-login-help icon-help circle-outline']")
+    public WebElement questionMark;
+
+    @FindBy(xpath = "//button[@class='btn btn-danger persistent-login-cta-btn']")
+    public WebElement closeQuestionMark;
+
+//    @FindBy(xpath = "//p[.='Use this option only on your own personal device to keep your account secure.']")
+//            //"h2[.='Keep Me Logged In']")
+//    public WebElement textFromQuestionMark;
+//
+     @FindBy(xpath = "//a[@class='btn btn-secondary-cta btn-block btn-default']")
+     public WebElement joinSkyMiles;
+
+
+    public void logInButtonClick() {
+        new LogInPage().logInButton.click();
+
+
+    }
+
+    public void incorrectUserAndPasswordInfo() {
+
+        Actions actions = new Actions(Driver.getDriver());
+
+        actions.click(userRow).sendKeys(ConfigReader.getProperty("invalidUsername")).sendKeys(Keys.ARROW_DOWN).perform();
+        actions.click(passwordRow).sendKeys(ConfigReader.getProperty("invalidPassword")).sendKeys(Keys.ENTER).perform();
+
+    }
+    public void scrollWindow(){
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("window.scrollBy(0,700)", "");
+    }
+
+    public void joinSkyMilesButton() {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("arguments[0].click()", new LogInPage().joinSkyMiles);
+    }
+}
---
+++ src/test/java/tests/LogInTest.java
@@ -0,0 +1,72 @@
+package tests;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+import pages.*;
+import utilities.Driver;
+import utilities.SeleniumUtils;
+
+import java.util.Collections;
+
+
+public class LogInTest extends TestBase {
+
+    @Test
+    public void verifyLogInButton() {
+
+        new LogInPage().logInButtonClick();
+        String expected = "Log In To Delta";
+        String pageSource = Driver.getDriver().getPageSource();
+     Assert.assertTrue(pageSource.contains(expected));
+    }
+
+    @Test
+    public void inccorectLogInfo(){
+        new LogInPage().logInButtonClick();
+        new LogInPage().incorrectUserAndPasswordInfo();
+        String expected ="please correct the 2 items indicated.";
+        String actual= new LogInPage().errorMessage.getText();
+        Assert.assertEquals(actual,expected);
+
+    }
+
+    @Test
+    public void checkBox(){
+
+       new LogInPage().logInButtonClick();
+       new LogInPage().checkbox.click();
+      // Assert.assertFalse(new LogInPage().checkbox.isSelected());
+    }
+
+    @Test
+    public void questionMarkOpen(){
+
+        new LogInPage().logInButtonClick();
+        new LogInPage().questionMark.click();
+
+       String expected ="Keep Me Logged In";
+       // String actual= new LogInPage().textFromQuestionMark.getText();
+        //System.out.println(actual);
+        String pageSource = Driver.getDriver().getPageSource();
+        Assert.assertTrue(pageSource.contains(expected));
+       new LogInPage().closeQuestionMark.click();
+
+    }
+
+     @Test
+    public void skyMilesButton(){
+
+        new LogInPage().logInButtonClick();
+        new LogInPage().scrollWindow();
+        new LogInPage().joinSkyMiles.click();
+        String expected ="Join SkyMiles";
+        String pageSource = Driver.getDriver().getPageSource();
+        Assert.assertTrue(pageSource.contains(expected));
+
+    }
+
+
+}
