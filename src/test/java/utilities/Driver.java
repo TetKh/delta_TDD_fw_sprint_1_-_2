5d04c2b709dc9906202a294305a2941805c05a86
Tetyana <tanyarunko@gmail.com>
2021-11-26 20:43:55 -0500

    parallel test

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -11,56 +11,56 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
-    /* utility class that initializes and quits the WebDriver and uses singleton pattern to prevent multiple initializations or quitting
-      of the WebDriver
-     */
 
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> drivers = new ThreadLocal<>();
+
 
     private Driver(){}
 
 
-    public static WebDriver getDriver(){
-        if(driver == null){
+    public static synchronized WebDriver getDriver(){
+        if(drivers.get() == null){
 
             String browser =  System.getProperty("browser");
             if(browser == null){
                 browser = ConfigReader.getProperty("browser");
             }
 
+
+
             switch (browser){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    drivers.set(new ChromeDriver());
                     break;
                 case "edge":
                     WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
+                    drivers.set(new EdgeDriver());
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    drivers.set(new FirefoxDriver());
                     break;
                 case "safari":
-                    driver = new SafariDriver();
+                    drivers.set(new SafariDriver());
                     break;
                 case "headlessChrome":
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--headless");
-                    driver = new ChromeDriver(chromeOptions);
+                    drivers.set(new ChromeDriver(chromeOptions));
                     break;
                 case "headlessFirefox":
                     WebDriverManager.firefoxdriver().setup();
                     FirefoxOptions firefoxOptions = new FirefoxOptions();
                     firefoxOptions.addArguments("--headless");
-                    driver = new FirefoxDriver(firefoxOptions);
+                    drivers.set(new FirefoxDriver(firefoxOptions));
                     break;
                 case "headlessEdge":
                     WebDriverManager.edgedriver().setup();
                     EdgeOptions edgeOptions = new EdgeOptions();
                     edgeOptions.addArguments("--headless");
-                    driver = new EdgeDriver(edgeOptions);
+                    drivers.set(new EdgeDriver(edgeOptions));
                     break;
                 default:
                     System.out.println("Invalid browser");
@@ -71,19 +71,17 @@ public class Driver {
         }
 
 
-           return  driver;
+        return  drivers.get();
     }
 
 
 
-    public static void quitDriver(){
-        if(driver != null){
-            driver.quit();
-            driver = null;
+    public static synchronized void quitDriver(){
+        if(drivers.get() != null){
+            drivers.get().quit();
+            drivers.remove();
         }
 
     }
-
-
 
 }
